2aba09d2293a884bcb0570e154296b51fd378980
Misha Goncharov <forsomeletters@gmail.com>
2020-08-28 03:21:38 +0500

    Сделал динамичным значение <base href="{folderBase}"> между тегами <head>

--- src/App.svelte
+++ src/App.svelte
@@ -3,18 +3,27 @@
 -->
 
 <script>
-  // Импуортируем компоненты маршрутизатора
+  // Импортируем компоненты маршрутизатора
   import { Router, Link, Route } from 'svelte-routing'
   
-  // Испортируем компоненты отдельных страниц
+  // Импортируем компоненты отдельных страниц
   import Index from './pages/Index.svelte'
   import About from './pages/About.svelte'
   import NotFound from './pages/NotFound.svelte'
 
   /* 
-    Константа, которая при сборке проекта заменяется Вебпаком на значение соответствующей переменной окружения. Служит для указания базового пути для случая, когда приложение запускается по более глубокому адресу. Например, чтобы проект открывался по ссылке example.com/subfolder вместо example.com, в .env-файле следует указать URL_BASE=subfolder
+    Константа, которая при сборке проекта заменяется Вебпаком на значение соответствующей переменной окружения. Служит для указания базового пути для случая, когда приложение запускается по более глубокому адресу. Например, чтобы проект открывался по ссылке example.com/subfolder вместо example.com, в .env-файле следует указать URL_BASE=subfolder.
+
+    Если /subfolder — не веритуальный УРЛ, а физическая папка, необходимо дополнительно указать FOLDER_BASE="/subfolder" (со слешем в начале)
   */
-  const base = URL_BASE || '/'
+  const URLBase = URL_BASE || '/'
+  const folderBase = FOLDER_BASE || '/'
+
+  // Вставляем <base href="{folderBase}"> первым элементом <head></head>
+  const base = document.createElement('base')
+  base.setAttribute('href', folderBase)
+  document.head.insertBefore(base, document.head.firstChild)
+  
 </script>
 
 <!-- 
@@ -25,8 +34,7 @@
   Роутер сам определяет наиболее специфический подходящий маршрут. Например, даже если на первое место поставить <Route path="*" />, этот маршрут будет задействован только в случае, если не найдется хотя бы одного с более точным адресом.
 -->
 
-
-<Router basepath="{base}">
+<Router basepath="{URLBase}">
   <Route path="/"><Index /></Route>
   <Route path="/about/"><About /></Route>
   <Route path="*"><NotFound /></Route>
